6d49595e0d18f6275245502b1407774032fd12b7
haavaav <43496370+haavaav@users.noreply.github.com>
2021-05-31 09:58:11 +0200

    04_AR_Feature_Extraction

---
+++ AR_modelling_3.0.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.ar_model import AutoReg
+from statsmodels.tsa.stattools import acf
+from scipy.stats import kurtosis, skew
+from warnings import simplefilter
+from handy import unpickle
+from handy import column_names
+from handy import choose_n_split
+simplefilter(action='ignore', category=FutureWarning)
+from warnings import filterwarnings
+filterwarnings('ignore')
+
+
+# # Handy
+# def column_names(name_list, number_of_columns, name_of_data):
+#     if name_of_data == 'AR_coef_':
+#         for idx in range(number_of_columns + 1):
+#             name = str(name_of_data) + str(idx)
+#             name_list.append(name)
+#     else:
+#         for idx in range(number_of_columns + 1):
+#             name = str(name_of_data) + str(idx)
+#             name_list.append(name)
+#     return name_list
+#
+# #Handy
+# def choose_n_split(data, str, n_splits):
+#     #Splits the dataframe into a chosen number of columns
+#     names = ['A', 'B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+#              'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'  , 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ',
+#              'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ',
+#              'BA', 'BB', 'BC', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BK', 'BL', 'BM', 'BN', 'BO', 'BP',
+#              'BQ', 'BR', 'BS', 'BT', 'BU', 'BV', 'BW', 'BX', 'BY', 'BZ', 'CA', 'CB', 'CC', 'CD', 'CE', 'CF',
+#              'CG', 'CH', 'CI', 'CJ', 'CK', 'CL', 'CM', 'CN', 'CO', 'CP', 'CQ', 'CR', 'CS', 'CT', 'CU', 'CV',
+#              'CW', 'CX', 'CY', 'CX']
+#     print(len(names))
+#     listen = np.array(data[str])
+#     length = len(listen) // n_splits
+#     #Lage evenly split data
+#     splitted_df = pd.DataFrame()
+#     for i in range (n_splits):
+#         if len(listen[length*i:length*(i+1)]) != length:
+#             print('Length of lists does not match, supressing the last set of data')
+#             break
+#         else:
+#             splitted_df[names[i]] = listen[length*i:length*(i+1)]
+#     if n_splits > len(names):
+#         print('Not enough names')
+#     return splitted_df
+
+def AR_basedata(baseline, p):
+    # Extracts (p+1) AR coefficients of a time series and the mean and standard deviation of its residual
+    ar_coef_list = []
+    ar_model = AutoReg(baseline, p)
+    model = ar_model.fit()
+    mean_b = np.mean(model.resid)
+    std_b = np.std(model.resid)
+    ar_coef_list.append(model.params)
+    return ar_coef_list, mean_b, std_b
+
+def AR_testdata(testdata, p, ar_coef_b, mean_b, std_b):
+    ar_model = AutoReg(testdata, p)
+    ar_model_fit_t = ar_model.fit()
+    ar_coef_t = ar_model_fit_t.params
+    ar_coef_list = list(ar_coef_t)
+
+
+    #Predicting data using undamaged AR coefficients
+    ar_model_fit = AutoReg.predict(ar_model, list(ar_coef_b[0]))
+    print((ar_coef_b[0]))
+    residual_test = testdata[p::] - ar_model_fit
+    mean_t = np.mean(residual_test)
+    std_t = np.std(residual_test, ddof=1)
+    skewness_t = skew(residual_test)
+    kurtosis_t = kurtosis(residual_test)
+    rms = np.sqrt(np.mean(residual_test ** 2))
+    correlation = acf(residual_test, fft=False, nlags=1)
+    pdf = (residual_test - mean_b) / std_b
+    amplitude = abs(residual_test).quantile(0.98)
+    outlier_r = 0
+    outlier_l = 0
+    for element in pdf:
+        if element < -1.96:
+            outlier_l += 1
+        elif element > 1.96:
+            outlier_r += 1
+
+    return mean_t, std_t, skewness_t, kurtosis_t, outlier_l, outlier_r, amplitude, rms, correlation[1], ar_coef_list
+
+
+# start_time = time.time()
+ar_order = 10
+n_splits = 100
+damage_list = [0, 1, 2, 3, 4, 5, 6]
+feature_names = ['AR_Mean', 'AR_STD', 'AR_Skewness', 'AR_Kurtosis', 'AR_Outlier_L', 'AR_Outlier_R', 'AR_Peak', 'AR_RMS', 'AR_Auto_Corr']
+feature_names = column_names(feature_names, ar_order, 'AR_Coef_')
+feature_names.append('Damage')
+# print(feature_names)
+# sensor_names = ['AL01']
+sensor_names = ['AL01', 'AL02', 'AL03', 'AL04', 'AL05', 'AL06', 'AL07',
+              'AL08', 'AL09', 'AL10', 'AL11', 'AL12', 'AL13', 'AL14', 'AL15', 'AL16',
+              'AL17', 'AL18', 'AL19', 'AL20', 'AL21', 'AL22', 'AL23', 'AL24', 'AL25',
+              'AL26', 'AL27', 'AL28', 'AL29', 'AL30', 'AL31', 'AL32', 'AL33', 'AL34',
+              'AL35', 'AL36', 'AL37', 'AL38', 'AL39', 'AL40', 'AG01x',
+                'AG02x', 'AG03x', 'AG04x', 'AG05x', 'AG06x', 'AG07x', 'AG08x', 'AG09x',
+                'AG10x', 'AG11x', 'AG12x', 'AG13x', 'AG14x', 'AG15x', 'AG16x', 'AG17x', 'AG18x', 'AG01z',
+                'AG02z', 'AG03z', 'AG04z', 'AG05z', 'AG06z', 'AG07z', 'AG08z',
+                'AG10z', 'AG11z', 'AG12z', 'AG13z', 'AG14z', 'AG15z', 'AG16z', 'AG17z', 'AG18z']
+sensor_list = ['sensor_base', 'sensor_05', 'sensor_11', 'sensor_23', 'sensor_26', 'sensor_29', 'sensor_32']
+
+
+baseline = unpickle(run='02')
+df05 = unpickle(run='05')
+df11 = unpickle(run='11')
+df23 = unpickle(run='23')
+df26 = unpickle(run='26')
+df29 = unpickle(run='29')
+df32 = unpickle(run='32')
+
+for sensor in sensor_names:
+    sensor_base = choose_n_split(baseline, sensor, n_splits)
+    sensor_05 = choose_n_split(df05, sensor, n_splits)
+    sensor_11 = choose_n_split(df11, sensor, n_splits)
+    sensor_23 = choose_n_split(df23, sensor, n_splits)
+    sensor_26 = choose_n_split(df26, sensor, n_splits)
+    sensor_29 = choose_n_split(df29, sensor, n_splits)
+    sensor_32 = choose_n_split(df32, sensor, n_splits)
+    ar_coef_b, mean_b, std_b = AR_basedata(baseline[sensor], ar_order)
+    res = []
+    for idx, element in enumerate(sensor_list):
+        # print(element)
+        for column in eval(element):
+            mean_t, sts_t, skewness_t, kurtosis_res, outlier_l, outlier_r, amplitude, rms, correlation, ar_coef_list = AR_testdata(eval(element)[column], ar_order, ar_coef_b, mean_b, std_b)
+            results = list([mean_t, sts_t, skewness_t, kurtosis_res, outlier_l, outlier_r, amplitude, rms, correlation] +  ar_coef_list)
+            results.append(damage_list[idx])
+            res.append(results)
+
+
+    features = pd.DataFrame(res, columns=feature_names)
+    print(features.to_string())
+    save_path = str('AR_results/AR_output_' + str(n_splits) + str(sensor) + '.pkl')
+    features.to_pickle(save_path)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
